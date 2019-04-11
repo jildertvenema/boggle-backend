de44eb903e68590f5c5f2bb5fa69e663cb7735c8
Jildert Venema <jildert.venema@hotmail.com>
2019-04-11 20:01:51 +0200

    should fix

--- src/modules/client-actions/database.js
+++ src/modules/client-actions/database.js
@@ -42,7 +42,7 @@ var DataBaseSingleton = (function () {
   var instance
 
   function createInstance() {
-      var object = new MongoDatabaseClient('mongodb+srv://mongo_user:7DFhfhnXxcLedXi7@cluster0-izty0.mongodb.net')
+      var object = new MongoDatabaseClient('mongodb+srv://mongo_user:7DFhfhnXxcLedXi7@cluster0-izty0.mongodb.net?retryWrites=true')
       return object;
   }
 
